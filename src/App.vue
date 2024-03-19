9d21922313fbb1fa57956bccbfff453d9cab740f
Alban-Luc <alban.luc@edu.univ-fcomte.fr>
2024-03-19 12:25:55 +0100

    footer

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router';
 import  Headerview  from './components/Header.vue';
-
+import Footerview from './components/Footer.vue';
 
 
 </script>
 
 <template>
   <Headerview />
+  <Footerview />
 </template>
 
