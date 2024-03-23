dc65a9085883de5f6beea432acd2daa7552b4357
Alban-Luc <alban.luc@edu.univ-fcomte.fr>
2024-03-23 15:23:51 +0100

    page_sml

--- src/views/SML.vue
+++ src/views/SML.vue
@@ -3,32 +3,40 @@
   <div class="mx-auto grid max-w-2xl grid-cols-1 items-center gap-x-8 gap-y-16 px-4 py-24 sm:px-6 sm:py-32 lg:max-w-7xl lg:grid-cols-2 lg:px-8">
     <div>
       <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Objectif</h2>
-      <p class="mt-4 text-gray-500">The walnut wood card tray is precision milled to perfectly fit a stack of Focus cards. The powder coated steel divider separates active cards from new ones, or can be used to archive important task lists.</p>
+      <p class="mt-4 text-gray-500">Réaliser un site peremettant de se retrouver physiquement.
+Le principe est simple, grace a la localisation de deux individu le site propose en temps réel un itinéraire pour se rejoindre.
+A chaque top de temps l'itinéraire est recalculé en fonction des déplacements</p>
 
       <dl class="mt-16 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-1 sm:gap-y-16 lg:gap-x-8">
-        <div class="border-t border-gray-200 pt-4">
-          <p class="font-medium text-gray-900">Présentation</p>
-          <p class="mt-2 text-sm text-gray-500">Designed by Good Goods, Inc.</p>
-        </div>
+
         <div class="border-t border-gray-200 pt-4">
           <p class="font-medium text-gray-900">Outils</p>
-          <p class="mt-2 text-sm text-gray-500">6.25&quot; x 3.55&quot; x 1.15&quot;</p>
+          <p class="mt-2 text-sm text-gray-500">Figma, Illustrator</p>
         </div>
 
         <div class="border-t border-gray-200 pt-4">
           <p class="font-medium text-gray-900">Lien</p>
-          <p class="mt-2 text-sm text-gray-500">Wood card tray and 3 refill packs</p>
+          <div class="flex mt-2">
+            <a href="https://www.behance.net/gallery/184419501/Share-my-loc" target="_blank">
+              <img src="/behance.png" alt="Behance" width="40" height="40">
+            </a>
+          </div>
+            <div class="mt-10">
+            <router-link to="/" class="bg-menu  text-white font-bold py-2 px-4 rounded inline-flex items-center">
+              Retour
+            </router-link>
+          </div>
         </div>
 
       </dl>
     </div>
     <div class="grid grid-cols-2 grid-rows-2 gap-4 sm:gap-6 lg:gap-8">
-      <img src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-01.jpg" alt="Walnut card tray with white powder coated steel divider and 3 punchout holes." class="rounded-lg bg-gray-100">
-      <img src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-02.jpg" alt="Top down view of walnut card tray with embedded magnets and card groove." class="rounded-lg bg-gray-100">
-      <img src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-03.jpg" alt="Side of walnut card tray with card groove and recessed card area." class="rounded-lg bg-gray-100">
-      <img src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-04.jpg" alt="Walnut card tray filled with cards and card angled in dedicated groove." class="rounded-lg bg-gray-100">
+      <img src="/sml2.png" alt="Walnut card tray with white powder coated steel divider and 3 punchout holes." class="rounded-lg bg-gray-100">
+      <img src="/sml3.png" alt="Top down view of walnut card tray with embedded magnets and card groove." class="rounded-lg bg-gray-100">
+      <img src="/sml4.png" alt="Side of walnut card tray with card groove and recessed card area." class="rounded-lg bg-gray-100">
     </div>
   </div>
 </div>
+
 
 </template>
